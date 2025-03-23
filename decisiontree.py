116f72a364b362888ce796beeea4937cde98e9c8
louis <zg24071@bristol.ac.uk>
2025-03-23 13:40:12 +0000

    discountinue

--- decisiontree.py
+++ decisiontree.py
@@ -13,7 +13,7 @@ df2 = pd.read_csv("ARASANFORPRESIDENT/train_person.csv")
 combinedf = pd.merge(df, df2, on = "CASENUM", how = "outer")
 
 # Drop unnecessary columns (modify as needed)
-combinedf = combinedf.drop(columns=["VIN", "C_ID_NO", "REGION", "VEHNO", "PJ", "CASENUM", "PERNO", "PSU", "AGE", "SEX", "EJECT", "VEH_ALCH", "NUM_INJV", "MODEL_YR", "VIOLATN", "MAX_VSEV", "HIT_RUN", "P_CRASH1", "IMPACT", "VEH_ROLE", "BODY_TYP", "MHENUM", "", "", ""])
+combinedf = combinedf.drop(columns=["VIN", "C_ID_NO", "REGION", "VEHNO", "PJ", "CASENUM", "PERNO", "PSU", "AGE", "SEX", "EJECT", "VEH_ALCH", "NUM_INJV", "MODEL_YR", "VIOLATN", "MAX_VSEV", "HIT_RUN", "P_CRASH1", "IMPACT", "VEH_ROLE", "BODY_TYP", "MHENUM", "FACTOR", "DRMAN_AV", "DR_DSTRD","AXLES","OCC_INVL","VROLE_I","VLTN_I"])
 
 # NOTE : NO MISSING VALUES TO FILL
 # Features is represented by numbers already
