5cb7090bd74fbbd7157c497c135a88b43a7c83f0
Lord Ezen <mo23274@bristol.ac.uk>
2025-03-23 12:44:57 +0000

    Dropbox

--- decisiontree.py
+++ decisiontree.py
@@ -5,11 +5,19 @@ from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor, plot_tre
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
+# PREPROCESSING
 # Load dataset from CSV file
 df = pd.read_csv("ARASANFORPRESIDENT/train_vehicle.csv")
 df2 = pd.read_csv("ARASANFORPRESIDENT/train_person.csv")
 
 combinedf = pd.merge(df, df2, on = "CASENUM", how = "outer")
 
-# Show first 5 rows  
-print(combinedf.head())    
+# Drop unnecessary columns (modify as needed)
+combinedf = combinedf.drop(columns=["VIN", "C_ID_NO", "REGION", "VEHNO", "PJ", "CASENUM", "PERNO", "PSU", "AGE", "SEX", "EJECT", "VEH_ALCH", "NUM_INJV", "MODEL_YR", "VIOLATN", "MAX_VSEV", "HIT_RUN", "P_CRASH1", "IMPACT", "VEH_ROLE", "BODY_TYP", "MHENUM"])
+
+# NOTE : NO MISSING VALUES TO FILL
+# Features is represented by numbers already
+
+#Deciding x and y axis
+x = 
+y = df("INJSEV_H")
