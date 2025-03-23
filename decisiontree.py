9d7c2adfc48c8a4694c6df4c5a8b1bb1e41e5897
Lord Ezen <mo23274@bristol.ac.uk>
2025-03-23 12:26:07 +0000

    Preprocessing

--- decisiontree.py
+++ decisiontree.py
@@ -4,3 +4,12 @@ import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor, plot_tree
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+
+# Load dataset from CSV file
+df = pd.read_csv("ARASANFORPRESIDENT/train_vehicle.csv")
+df2 = pd.read_csv("ARASANFORPRESIDENT/train_person.csv")
+
+combinedf = pd.merge(df, df2, on = "CASENUM", how = "outer")
+
+# Show first 5 rows  
+print(combinedf.head())    
